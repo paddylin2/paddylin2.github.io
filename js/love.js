840963d44b8957447956f6a20ca65629b02671b0
PaddyLin-xum <2841738015@qq.com>
2020-07-22 00:27:00 +0800

    Site updated: 2020-07-22 00:26:56

--- js/love.js
+++ js/love.js
@@ -1,1 +1,4 @@
-$(document).ready((function(n){$(".copyright").html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> PaddyLin')}));
+// 设置页脚博主会动的心
+$(document).ready(function(e){
+    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> PaddyLin');
+})
