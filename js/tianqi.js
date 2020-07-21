2b27bbef7038e5e69a31cde664e65a4e6134e01d
PaddyLin-xum <2841738015@qq.com>
2020-07-22 00:06:52 +0800

    Site updated: 2020-07-22 00:06:48

--- js/tianqi.js
+++ js/tianqi.js
@@ -1,14 +1,1 @@
-
-(function(a,h,g,f,e,d,c,b){b=function(){d=h.createElement(g);c=h.getElementsByTagName(g)[0];d.src=e;d.charset="utf-8";d.async=1;c.parentNode.insertBefore(d,c)};a["SeniverseWeatherWidgetObject"]=f;a[f]||(a[f]=function(){(a[f].q=a[f].q||[]).push(arguments)});a[f].l=+new Date();if(a.attachEvent){a.attachEvent("onload",b)}else{a.addEventListener("load",b,false)}}(window,document,"script","SeniverseWeatherWidget","//cdn.sencdn.com/widget2/static/js/bundle.js?t="+parseInt((new Date().getTime() / 100000000).toString(),10)));
-window.SeniverseWeatherWidget('show', {
-  flavor: "bubble",
-  location: "WS7GQBRNR6V8",
-  geolocation: true,
-  language: "auto",
-  unit: "c",
-  theme: "auto",
-  token: "341136ed-8b04-4e7c-a2c9-d4e95eabd8a7",
-  hover: "enabled",
-  container: "tp-weather-widget"
-})
-
+!function(e,t,n,a,o,i,c,d){d=function(){i=t.createElement(n),c=t.getElementsByTagName(n)[0],i.src=o,i.charset="utf-8",i.async=1,c.parentNode.insertBefore(i,c)},e.SeniverseWeatherWidgetObject=a,e[a]||(e[a]=function(){(e[a].q=e[a].q||[]).push(arguments)}),e[a].l=+new Date,e.attachEvent?e.attachEvent("onload",d):e.addEventListener("load",d,!1)}(window,document,"script","SeniverseWeatherWidget","//cdn.sencdn.com/widget2/static/js/bundle.js?t="+parseInt(((new Date).getTime()/1e8).toString(),10)),window.SeniverseWeatherWidget("show",{flavor:"bubble",location:"WS7GQBRNR6V8",geolocation:!0,language:"auto",unit:"c",theme:"auto",token:"341136ed-8b04-4e7c-a2c9-d4e95eabd8a7",hover:"enabled",container:"tp-weather-widget"});
